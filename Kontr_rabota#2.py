7124c553771dbd1bb5855c3e1982a94f3969220c
Malekk0 <44036411+Malekk0@users.noreply.github.com>
2019-05-12 17:15:54 +0300

    исправлена кр№2

--- Kontr_rabota#2.py
+++ Kontr_rabota#2.py
@@ -1,13 +1,11 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
-import sklearn.datasets as ds
-from sklearn.model_selection import train_test_split
 import math
 
 
 def sigmoid(x):
-  return 1/(1+math.exp(-x))
+
+    return 1 / (1 + math.exp(-x))
 
 
 
@@ -16,38 +14,35 @@ df = pd.read_csv(file)
 
 data = df.values
 
-X=data[:,:9]
+X=data[:,:8]
 Y=data[:,8:]*2-1
 l=Y.shape[0]
 X = np.concatenate([np.ones((X.shape[0],1)), X], axis=1)
 
 
+w = np.random.normal(0.0, 1.0, 9)
 
-w=np.zeros(10)
+n = 0.2
+for k in range(300):
+  for j in range(9):
+    sum = 0
+    for i in range(l):
+      sum += Y[i] * X[i, j] * sigmoid(-Y[i] * w.dot(X[i]))
 
-n=0.1
-
-for k in range(500):
-  sum=0
-  for j in range(10):
-    for i in range (l):
-      sum+=Y[i]*X[i,j]*sigmoid(-Y[i]*w.dot(X[i]))
-
-  w[j]=w[j]+n*(1.0/l)*sum
+w[j] = w[j] + n * (1.0 / l) * sum
 
 y_pr=X.dot(w)
 y_pr_b=[]
 
 for x in y_pr:
-  y_pr_b.append(1 if x<0 else 0)
+  y_pr_b.append(1 if x>0 else -1)
 
-
-print(y_pr_b)
 
 sum=0
 
 for i in range(l):
-  sum+=1 if Y[i]==y_pr_b[i]*2-1 else 0
+  sum+=1 if Y[i]==y_pr_b[i] else 0
 
 A=sum/l
 print(A)
+
